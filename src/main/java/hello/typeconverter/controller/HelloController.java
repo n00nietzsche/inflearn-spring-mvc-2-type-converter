e25454d839c53e1342169421e8cfdb9ff96ced0f
Jake Seo <n00nietzsche@gmail.com>
2021-09-03 08:23:56 +0900

    스프링 MVC 2편 - 스프링 타입 컨버터 #1 프로젝트 생성 및 소개

---
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -0,0 +1,27 @@
+package hello.typeconverter.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello-v1")
+    public String helloV1(HttpServletRequest request) {
+        // .getParameter()는 기본으로는 문자 타입으로 조회함
+        String data = request.getParameter("data");
+        // 숫자 타입으로 변경
+        Integer intValue = Integer.valueOf(data);
+        System.out.println("intValue = " + intValue);
+        return "ok";
+    }
+
+    @GetMapping("/hello-v2")
+    public String helloV2(@RequestParam Integer data) {
+        // 형변환되어 그대로 출력 가능
+        System.out.println("intValue = " + data);
+        return "ok";
+    }
+}
